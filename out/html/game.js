a9de8d499cdd3bed0a93a5defbd9777cdc0dd16b
Meese33 <50782323+Meese33@users.noreply.github.com>
2026-01-13 21:11:40 -0700

    Update game.js

--- out/html/game.js
+++ out/html/game.js
@@ -16,7 +16,7 @@
     // Add your custom code here.
   };
 
-  var TITLE = "Social Democracy: An Alternate History" + '_' + "Autumn Chen";
+  var TITLE = "Stocks and Stonrd: An Alternate History" + '_' + "Autumn Chen" + '_' + "Sabotabby_";
 
   // the url is a link to game.json
   // test url: https://aucchen.github.io/social_democracy_mods/v0.1.json
